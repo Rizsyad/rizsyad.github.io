329088f66ed8f3e3010564f664dea45ce1262b2f
rizsyad <rizsyad@gmail.com>
2023-02-21 10:38:53 +0700

    Updates

---
+++ static/js/945.d388ecb3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_protofolio=self.webpackChunkreact_protofolio||[]).push([[945],{945:function(r,e,a){a.r(e);var o=a(457);e.default=function(r){var e=r.children,a=r.className;return a="bg-transparent border-2 mb-5 border-[#494ea1] rounded-lg shadow-xl md:mb-0 ".concat(a),(0,o.jsx)("div",{className:a,children:e})}}}]);
+//# sourceMappingURL=945.d388ecb3.chunk.js.map
